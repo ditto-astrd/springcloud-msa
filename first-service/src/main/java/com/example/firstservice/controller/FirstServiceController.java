632d2e47d8f456332f5c33ce2330acec74cf2c31
ditto-astrd <astrid.prcss@gmail.com>
2024-09-16 17:09:02 +0900

    feat : LoadBalancer까지 구현

--- first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
@@ -1,6 +1,9 @@
 package com.example.firstservice.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +26,9 @@ public class FirstServiceController {
   }
 
   @GetMapping("/check")
-  public String check() {
-    return "Hi, there. This is a message from First Service.";
+  public String check(HttpServletRequest request) {
+    log.info("Server port = {}", request.getServerPort());
+    return String.format("Hi, there. This is a message from First Service on PORT : %s",request.getServerPort());
   }
 
 }
